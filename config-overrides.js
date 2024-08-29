6b7c853d97f8609e61c3861885bd4ee6b43a0350
nexid-base <nexid.base@gmail.com>
2024-08-29 16:44:13 -0600

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  // Add polyfills
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify"),
+    "url": require.resolve("url")
+  };
+
+  // Add plugins
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]);
+
+  return config;
+}
